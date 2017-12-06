f94e8341f1b26b3c1deffe7098f14a7f05c7b7e1
gregos <troch.gregory@gmail.com>
2017-12-06 15:17:58 +0100

    Ajout d'une fonction dans le fichier function.js

---
+++ function.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+   function Message() {
+       var msg="Message sur la ligne 1.nMessage sur la ligne 2.n...";
+       console.log(msg)
+       alert(msg);
+   }
+</script>
